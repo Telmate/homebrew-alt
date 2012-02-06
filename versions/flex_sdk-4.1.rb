4a7d8917b97774dabc71fb3592e8350df49418c2
Jamie Alessio <jamie@telmate.com>
2012-02-06 15:13:26 -0800

    adding two flex versions for development

---
+++ versions/flex_sdk-4.1.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class FlexSdk < Formula
+  url 'http://fpdownload.adobe.com/pub/flex/sdk/builds/flex4/flex_sdk_4.1.0.16076A.zip'
+  homepage 'http://opensource.adobe.com/wiki/display/flexsdk/Flex+SDK'
+  md5 'd6c1975042215e24f9c177363707c5c1'
+
+  def install
+    libexec.install Dir['*']
+  end
+
+  def caveats; <<-EOS.undent
+    To use the SDK you will need to:
+
+    (a) add the bin folder to your $PATH:
+      #{libexec}/bin
+
+    (b) set $FLEX_HOME:
+      export FLEX_HOME=#{libexec}
+
+    (c) add the tasks jar to ANT:
+      mkdir -p ~/.ant/lib
+      ln -s #{libexec}/ant/lib/flexTasks.jar ~/.ant/lib
+    EOS
+  end
+end
