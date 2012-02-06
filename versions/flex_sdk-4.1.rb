57a1decd60279c08703523712d9242f6c9157f2f
Jamie <jamie@stoic.net>
2012-02-06 15:22:33 -0800

    Get class name right.

--- versions/flex_sdk-4.1.rb
+++ versions/flex_sdk-4.1.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class FlexSdk < Formula
+class FlexSdk41 < Formula
   url 'http://fpdownload.adobe.com/pub/flex/sdk/builds/flex4/flex_sdk_4.1.0.16076A.zip'
   homepage 'http://opensource.adobe.com/wiki/display/flexsdk/Flex+SDK'
   md5 'd6c1975042215e24f9c177363707c5c1'
